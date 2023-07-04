59a9d5a4eebbf7a4c87f1f0ed08f4bdd9628c876
Astrid Serrano <yari12_94@hotmail.com>
2023-07-04 15:39:50 -0600

    server

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 app.use(cors());
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
 });
 
 var taskRouter = express.Router();
@@ -21,22 +21,26 @@ taskRouter
     return res.json(tasks);
   })
   .post(function (req, res) {
-    tasks.push({
+    const newTask = {
       ...req.body,
       id: uuid(),
-    });
-    return res.json(tasks);
+    };
+    tasks.push(newTask);
+    return res.json(newTask);
   });
+
 taskRouter.route("/:task_id")
   .get(function (req, res) {
     const id = req.params.task_id
     return res.json(tasks.find((t) => t.id === id) ?? {});
   })
+
   .delete(function (req, res) {
     const id = req.params.task_id
     tasks = tasks.filter((t) => t.id !== id)
     return res.json({});
   })
+  
   .put(function (req, res) {
     const id = req.params.task_id
     for (let index in tasks) {
